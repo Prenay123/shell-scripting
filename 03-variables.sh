a8ecd1e3ac68c471b434e759bcd54ff06bba26f9
Prenay123 <prenaysaidiska@gmail.com>
2025-10-29 15:09:57 +0530

    conversations

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,8 +3,8 @@
 person1=prenay
 person2=ramya
 
-echo "person1:: hi person2 how r you"
-echo "person2:: hi person1 i am good, how about you"
-echo "person1:: i am also good, what are you doing these days"
-echo "person2:: i am learning shell scripting"
-echo "person1:: oh thats great, shell scripting is very useful"
+echo "$person1:: hi $person2 how r you"
+echo "$person2:: hi $person1 i am good, how about you"
+echo "$person1:: i am also good, what are you doing these days"
+echo "$person2:: i am learning shell scripting"
+echo "$person1:: oh thats great, shell scripting is very useful"
