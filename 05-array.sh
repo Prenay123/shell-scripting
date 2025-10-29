18029e28537358916292a2c5fe478cf472f53b49
Prenay123 <prenaysaidiska@gmail.com>
2025-10-29 15:43:59 +0530

    conversations

--- 05-array.sh
+++ 05-array.sh
@@ -2,4 +2,8 @@
 
 MOVIES=("Inception" "The Dark Knight" "Interstellar" "The Matrix" "Pulp Fiction" "Fight Club")
 
-echo "my favorite movies are: ${MOVIES[1]}"
+echo "my favorite movie is: ${MOVIES[1]}"
+
+echo "my favorite movie is: ${MOVIES[1]} and ${MOVIES[3]}"
+
+echo "All movies are: ${MOVIES[@]}"
