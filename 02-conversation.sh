a957a7f836a8443b3f8c8246c10350625ef757ea
Prenay123 <prenaysaidiska@gmail.com>
2025-10-29 15:05:48 +0530

    conversations

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+echo "prenay:: hi ramya how r you"
+echo "ramya:: hi prenay i am good, how about you"
+echo "prenay:: i am also good, what are you doing these days"
+echo "ramya:: i am learning shell scripting"
+echo "prenay:: oh thats great, shell scripting is very useful"
---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+person1=prenay
+person2=ramya
+
+echo "prenay:: hi ramya how r you"
+echo "ramya:: hi prenay i am good, how about you"
+echo "prenay:: i am also good, what are you doing these days"
+echo "ramya:: i am learning shell scripting"
+echo "prenay:: oh thats great, shell scripting is very useful"
